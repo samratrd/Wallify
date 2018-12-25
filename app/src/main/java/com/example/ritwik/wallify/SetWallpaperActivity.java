258302eb46f3553283e3eb92f42fccd1c3a56489
samratrd <developer.samrat16@gmail.com>
2018-12-26 01:11:11 +0530

    layout and design updated

--- app/src/main/java/com/example/ritwik/wallify/SetWallpaperActivity.java
+++ app/src/main/java/com/example/ritwik/wallify/SetWallpaperActivity.java
@@ -2,14 +2,22 @@ package com.example.ritwik.wallify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class SetWallpaperActivity extends AppCompatActivity {
 
     private Bundle extras;
     private ImageView imageView;
+    private FloatingActionMenu floatingActionMenu;
+    private FloatingActionButton button_setas, button_share, button_download;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,11 @@ public class SetWallpaperActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_wallpaper);
 
         imageView = findViewById(R.id.image_preview);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Wallify");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         extras = getIntent().getExtras();
 
@@ -28,5 +41,48 @@ public class SetWallpaperActivity extends AppCompatActivity {
 
 
         }
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getSupportActionBar().isShowing()){
+                    getSupportActionBar().hide();
+                }
+                else
+                {
+                    getSupportActionBar().show();
+                }
+
+            }
+        });
+
+        floatingActionMenu = findViewById(R.id.floating_action_menu);
+        button_download = findViewById(R.id.download);
+        button_setas = findViewById(R.id.set_as);
+        button_share = findViewById(R.id.share);
+
+        button_download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // handle download
+            }
+        });
+
+
+        button_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //handle share
+            }
+        });
+
+
+        button_setas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set wallpaper
+            }
+        });
     }
 }
